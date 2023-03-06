56a318a8c94a86c80bfe1913dab39dbf707f1980
MilkLiver <neil.yu@dynasafe.com.tw>
2023-03-06 07:18:13 +0000

    update inventory default user

--- controller/startup.sh
+++ controller/startup.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 echo "$VM_IP node01 node02 controller" >>/etc/hosts
 
+# --------------------
 cat <<EOF >/workspace/ansible.cfg
 [defaults]
 inventory=./inventory
-remote_user=student
 ask_pass=false
 private_key_file=/workspace/id_rsa
 
@@ -15,6 +15,7 @@ become_user=root
 become_ask_pass=false
 EOF
 
+# --------------------
 cat <<EOF >/workspace/inventory
 [ansible_node01]
 node01
@@ -23,10 +24,15 @@ node01
 node02
 
 [ansible_node01:vars]
+ansible_user=node01
 ansible_port=$ANSIBLE_EDU_NODE01_PORT
 
 [ansible_node02:vars]
+ansible_user=node02
 ansible_port=$ANSIBLE_EDU_NODE02_PORT
 EOF
+# --------------------
+
+export $ANSIBLE_EDU_NODE01_PORT
 
 tail -f /dev/null

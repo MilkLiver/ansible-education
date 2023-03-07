df8cad34f5a9f804f28a2810b352a949b93e4c92
MilkLiver <neil.yu@dynasafe.com.tw>
2023-03-07 03:43:47 +0000

    add some default vars

--- ansible_education_create_env.sh
+++ ansible_education_create_env.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 STUDENT_NUMBERS=3
-VM_IP=xxx.xxx.xxx.xxx
-EDUCATION_IMAGE_VERSION=1.5.0
+VM_IP=192.168.50.111
+EDUCATION_IMAGE_VERSION=1.7.0
 
 WORKSPACE_ROOT_DIR=/opt/ansible-edu
 ANSIBLE_EDU_CONNECT_SHELL_DIR=/opt/ansible-edu/connectController
